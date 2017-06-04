f92fd4602798e2f0f5fe8951d3c10fe5de5a7722
Jonathan Share <jonny@sharebear.co.uk>
2017-06-04 20:15:10 +0200

    feat: added support for deserializing json with configured objectmapper
    This adds methods to the JsonTransformer that is wrapping jackson in
    order to correctly deserialize javaslang and jsr310 types.
    
    Only trivially tested as this will be covered by testing of the
    Endpoints that use it.

--- src/main/java/uk/co/sharebear/portfolio/ordermanagement/api/library/sparkjava/JsonTransformer.java
+++ src/main/java/uk/co/sharebear/portfolio/ordermanagement/api/library/sparkjava/JsonTransformer.java
@@ -1,7 +1,10 @@
 package uk.co.sharebear.portfolio.ordermanagement.api.library.sparkjava;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
 
 import javaslang.jackson.datatype.JavaslangModule;
 
@@ -15,5 +18,13 @@ public class JsonTransformer {
 
   public static String toJson(Object model) throws Exception {
     return MAPPER.writeValueAsString(model);
+  }
+
+  public static <T> T fromJson(String content, Class<T> valueType) throws IOException {
+    return MAPPER.readValue(content, valueType);
+  }
+
+  public static <T> T fromJson(String content, TypeReference<T> valueType) throws IOException {
+    return MAPPER.readValue(content, valueType);
   }
 }
--- src/test/java/uk/co/sharebear/portfolio/ordermanagement/api/library/sparkjava/JsonTransformerTest.java
+++ src/test/java/uk/co/sharebear/portfolio/ordermanagement/api/library/sparkjava/JsonTransformerTest.java
@@ -1,7 +1,10 @@
 package uk.co.sharebear.portfolio.ordermanagement.api.library.sparkjava;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
 import org.junit.Test;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 import javaslang.control.Option;
@@ -27,10 +30,38 @@ public class JsonTransformerTest {
   }
 
   @Test
+  public void shouldDeserializeNullOptionValueAsOptionNone() throws IOException {
+    final Option result = JsonTransformer.fromJson("null", Option.class);
+
+    // replace with assertion library if time to mess with mvn
+    assertThat(result.isEmpty(), is(true));
+  }
+
+  @Test
+  public void shouldDeserializeNonNullOptionValueAsOptionOf() throws IOException {
+    final Option<String> result = JsonTransformer.fromJson(
+        "\"testvalue\"",
+        new TypeReference<Option<String>>() {
+        }
+    );
+
+    // replace with assertion library if time to mess with mvn
+    assertThat(result.isDefined(), is(true));
+    assertThat(result.get(), is(equalTo("testvalue")));
+  }
+
+  @Test
   public void shouldSerializeLocalDateWithISOFormat() throws Exception {
     final LocalDate testDate = LocalDate.of(2021, 3, 15);
     final String result = JsonTransformer.toJson(testDate);
     final String expected = "\"2021-03-15\"";
+    assertThat(result, is(equalTo(expected)));
+  }
+
+  @Test
+  public void shouldDeserializeLocalDateWithISOFormat() throws IOException {
+    final LocalDate result = JsonTransformer.fromJson("\"2021-03-15\"", LocalDate.class);
+    final LocalDate expected = LocalDate.of(2021, 3, 15);
     assertThat(result, is(equalTo(expected)));
   }
 }
